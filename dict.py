e57f4612dde6ca4c7776f9a2b8de9da3146332c7
Janhvi Gupta <janhvi.xgupta@gmail.com>
2026-06-08 09:50:17 +0530

    Added new Python practice programs

--- dict.py
+++ dict.py
@@ -8,18 +8,38 @@
 # null_dict = {}
 # print(null_dict)
 
-student = {
-    "name": "Janhvi Gupta",
-    "subjects": {
-        "phy": 97,
-        "chem": 98,
-        "math": 99
-    }
-}
-# print(student["name1"]) #shows error
-# print(student.get("name1")) #gives none
+# student = {
+#     "name": "Janhvi Gupta",
+#     "subjects": {
+#         "phy": 97,
+#         "chem": 98,
+#         "math": 99
+#     }
+# }
+# # print(student["name1"]) #shows error
+# # print(student.get("name1")) #gives none
+# new_dict = {"place": "Gurgaon"}
+# student.update(new_dict)
+# print(student)
 
-new_dict = {"place": "Gurgaon"}
-student.update(new_dict)
+# word_meaning = {
+#     "table": {
+#         "a piece of furniture",
+#         "list of facts & figures",
+#     },
+#     "cat": "a small animal"
+# }
 
-print(student)
+# print(word_meaning)
+
+#WAP to enter marks of 3 subjects from the user and store them in a dictonary. start with an empty dict and add one by one.
+marks = {}
+
+x = int(input("Enter marks of Physics: "))
+marks.update({"phy": x})
+x = int(input("Enter marks of Maths: "))
+marks.update({"math": x})
+x = int(input("Enter marks of Chemistry: "))
+marks.update({"chem": x})
+
+print(marks)
--- set.py
+++ set.py
@@ -1,5 +1,25 @@
-collection = {3, 4, 9.81, 377, 4, 4, 5}
+# #you are given a list of subjects for students. assume one class is required for 1 subject. how many classrooms are needed by all students.
 
-print(collection)
-print(type(collection))
-print(len(collection)) 
+# classroom = set()
+# classroom.add("python")
+# classroom.add("java")
+# classroom.add("C++")
+# classroom.add("python")
+# classroom.add("javascript")
+# classroom.add("java")
+# classroom.add("python")
+# classroom.add("java")
+# classroom.add("C++")
+# classroom.add("C")
+
+# print(classroom)
+# print(len(classroom))
+# #5 classrooms are needed
+
+subjects = {
+    "python", "java", "C++", "python", "javascript",
+    "java", "python", "java", "C++", "C"
+}
+print(subjects)
+print(len(subjects))
+#5 classrooms are needed 
